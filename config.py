5c53e7b2f0bee4ae051e91c58f27f7ec95b3db80
fishnsotong <wwzyeo@gmail.com>
2024-09-30 13:35:39 +0800

    added hyperparameters from previous run

--- config.py
+++ config.py
@@ -14,17 +14,26 @@ MODEL_SAVE_DIR = "models"
 HYPERPARAMS = {
     "logreg": {
         "C": 1.0,
-        "max_iter": 1000
+        "max_iter": 1000,
+        "penalty": "l2",
+        "solver": "liblinear",
     },
     "random_forest": {
-        "n_estimators": 100,
+        "n_estimators": 200,
         "max_depth": 10,
+        "min_samples_leaf": 1,
+        "min_samples_split": 2,
+        "bootstrap": False,
         "random_state": 42
     },
     "xgboost": {
-        "n_estimators": 100,
-        "max_depth": 6,
+        "n_estimators": 500,
+        "max_depth": 10,
         "learning_rate": 0.1,
+        "gamma": 0.1,
+        "reg_lambda": 100,
+        "subsample": 0.8,
+        "colsample_bytree": 0.8,
         "random_state": 42
     }
 }
